a16bf5db06c3a0104e96aa4813d5983f47853b13
Al Snow <jasnow@hotmail.com>
2018-12-14 11:36:50 -0500

    Fixed ERB warning

--- lib/gemsurance/formatters/yml.rb
+++ lib/gemsurance/formatters/yml.rb
@@ -3,7 +3,7 @@ module Gemsurance
     class Yml < Base
       def format
         @extension = "yml"
-        ERB.new(File.read(output_path), nil, '-').result(binding)
+        ERB.new(File.read(output_path), trim_mode: '-').result(binding)
       end
     end
   end
